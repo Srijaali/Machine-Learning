c5c33dae3ecf4c97ceade4a9a6e4a7bea25105bf
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2025-10-04 15:07:40 +0500

    Update mid_prac.py

--- LAB/mid_prac.py
+++ LAB/mid_prac.py
@@ -5,3 +5,12 @@ print('index of Shortness of Breath: ', cancer.columns.get_loc('Shortness of Bre
 print('index of Frequent Cold: ', cancer.columns.get_loc('Frequent Cold'))
 sample2 = cancer.iloc[-20:, 18:23]
 sample2.to_csv('sample2.csv')
+
+# heatmap:
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize=(15,10))
+sns.heatmap(cancer.corr(numeric_only=True), annot=True, cmap="coolwarm")
+plt.title("Correlation Heatmap of Features")
+plt.show()
